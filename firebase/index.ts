1fadaf0c6162d2246808c3af8d6d15768b85fad1
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2022-02-11 13:25:32 +0330

    install materil ui

---
+++ firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkClnTNJCsKk2wo_59PRMkrISCKYfXpZ0",
+  authDomain: "tactical-gaming-d61ad.firebaseapp.com",
+  projectId: "tactical-gaming-d61ad",
+  storageBucket: "tactical-gaming-d61ad.appspot.com",
+  messagingSenderId: "769094007489",
+  appId: "1:769094007489:web:71028f3ba483e4745a7240"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
